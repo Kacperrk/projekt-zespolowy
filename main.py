300c99cefccf508000c81bd5e3e5c85013634e9c
Kacperrk <130840734+Kacperrk@users.noreply.github.com>
2025-04-15 02:09:36 +0200

    Added main() function

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from gui import KomiwojazerApp
 
-if __name__ == "__main__":
+def main():
     app = QApplication(sys.argv)
     window = KomiwojazerApp()
     window.resize(1000, 600)
     window.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
