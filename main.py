0522ec4e9e19ebaa32340089e44c751f54ace818
JNSau <148083836+JNSau@users.noreply.github.com>
2025-04-29 12:07:54 +0200

    Update and some upgrades for main.py

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from gui import KomiwojazerApp
 
+
+def create_application():
+    return QApplication(sys.argv)
+
+
 def main():
-    app = QApplication(sys.argv)
-    window = KomiwojazerApp()
-    window.resize(1000, 600)
-    window.show()
+    app = create_application()
+    main_window = KomiwojazerApp()
+    main_window.resize(1000, 600)
+    main_window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
