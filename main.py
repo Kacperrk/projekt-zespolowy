ddf39ce814aa35edb6c3b9ceb2d7a7daf3dfbcf0
Kacperrk <kacper0492@wp.pl>
2025-05-03 22:42:11 +0200

    Add typing to main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
+from typing import NoReturn
 from PyQt5.QtWidgets import QApplication
 from gui import KomiwojazerApp
 
 
-def create_application():
+def create_application() -> QApplication:
     return QApplication(sys.argv)
 
 
-def main():
-    app = create_application()
-    main_window = KomiwojazerApp()
+def main() -> NoReturn:
+    app: QApplication = create_application()
+    main_window: KomiwojazerApp = KomiwojazerApp()
     main_window.resize(1000, 600)
     main_window.show()
     sys.exit(app.exec_())
