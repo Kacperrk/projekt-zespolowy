3940864c354a14f73f04f71ce0564b98116af421
JNSau <148083836+JNSau@users.noreply.github.com>
2025-05-05 21:06:46 +0200

    update main.py
    Usunięcie NoReturn: Ponieważ main() nie zwraca niczego, a jest funkcją, która kończy wykonanie programu.
    Optymalizacja sys.exit(): Teraz po wywołaniu app.exec_() aplikacja sama kończy działanie bez potrzeby używania sys.exit(). 
    
    Inicjalizacja QApplication i main_window: Dzięki prostemu połączeniu tych dwóch kroków kod jest bardziej zwięzły i przejrzysty.

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 import sys
-from typing import NoReturn
 from PyQt5.QtWidgets import QApplication
 from gui import KomiwojazerApp
 
-
-def create_application() -> QApplication:
-    return QApplication(sys.argv)
-
-
-def main() -> NoReturn:
-    app: QApplication = create_application()
-    main_window: KomiwojazerApp = KomiwojazerApp()
+def main() -> None:
+    app = QApplication(sys.argv)  # Tworzenie aplikacji
+    main_window = KomiwojazerApp()  # Tworzenie głównego okna
     main_window.resize(1000, 600)
     main_window.show()
-    sys.exit(app.exec_())
-
+    app.exec_()  # Uruchomienie głównej pętli aplikacji
 
 if __name__ == "__main__":
     main()
